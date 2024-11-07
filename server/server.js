a1932b69c76fca7a5d95faa02af52e445251345d
Rohit Pandey <rd8614196@gmail.com>
2024-11-07 16:40:36 +0530

    Allowed all the frontend urls

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://linkk.netlify.app",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
