87c964bfbbd204b881b4098a46572b97d403edbd
Rohit Pandey <rd8614196@gmail.com>
2024-11-07 18:00:54 +0530

    Error fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import LinkList from './components/LinkList';
 import { store } from './app/store';
-import { setupSocketListeners } from './features/linksSlice.js'
+import { setupSocketListeners } from './features/linksSlice.jsx'
 setupSocketListeners(store)
 
 const App = () => {
