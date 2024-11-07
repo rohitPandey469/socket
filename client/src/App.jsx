706a203c7c02e1183bdb291e7d80db3ec9d4b75c
Rohit Pandey <rd8614196@gmail.com>
2024-11-07 16:49:26 +0530

    Check Now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import LinkList from './components/LinkList';
 import { store } from './app/store';
+import { setupSocketListeners } from './features/linksSlice.js'
+setupSocketListeners(store)
 
 const App = () => {
     return (
